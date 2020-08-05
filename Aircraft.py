0679fb10ed857a20259963bd330db3780f74b33c
Robert A Alfieri <balfieri@nvidia.com>
2020-08-05 10:09:05 -0400

    comment

--- Aircraft.py
+++ Aircraft.py
@@ -1,5 +1,8 @@
-# Known Aircraft Types and Their Performance Characteristics
-# Overrides for Specific Tail Numbers
+# Aircraft.py - Known Aircraft Types and Their Performance Characteristics
+#               Overrides for Specific Tail Numbers
+#
+# Feel free to ADD your own types and tails to this.  
+# Just don't delete what is already here.
 #
 types = { 'C172S': {} }
 tails = { }
