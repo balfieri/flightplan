57e6dc8d9c2f43e9ca586d462b46c5b1015851dc
Robert A Alfieri <balfieri@nvidia.com>
2020-11-15 21:35:33 -0500

    added faa info parse routine

--- rawdata/make_rawdata.py
+++ rawdata/make_rawdata.py
@@ -27,8 +27,11 @@ def cmd( c ):
     else:
         return ''
 
+m = None 
 def match( s, pattern ): 
-    return re.compile( pattern ).match( s )
+    global m
+    m = re.compile( pattern ).match( s )
+    return m
 
 def subst( s, pattern, subst ):
     return re.sub( pattern, subst, s )
@@ -45,6 +48,64 @@ def latlon_to_decimal( latlon ):
     latlon = float(latlon[:-1]) / 3600.0
     if ch == 'S' or ch == 'W': latlon = -latlon
     return latlon
+
+def parse_faa_text( id ):
+    #--------------------------------------------------------------
+    # Download more detailed information from faa.gov, but don't 
+    # do this if we have alread done it for a particular airport.
+    # Convert html to text and write to the .faa.text.out file.
+    #--------------------------------------------------------------
+    if not os.path.exists( f'airports/{id}.faa.out' ):
+        for t in range(10):
+            time.sleep( 1 )
+            c = f'wget -O - https://nfdc.faa.gov/nfdcApps/services/ajv5/airportDisplay.jsp?airportId={id} > airports/{id}.faa.out'
+            print( c )
+            info = subprocess.run( c, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
+            if info.returncode == 0: break
+            cmd( f'rm -f airports/{id}.faa.out' )
+            if t == 9: print( f'could not get FAA airport data for {id}' )
+    if os.path.exists( f'airports/{id}.faa.out' ) and not os.path.exists( f'airports/{id}.faa.text.out' ):
+        cmd( f'html2text airports/{id}.faa.out > airports/{id}.faa.text.out' )
+
+    if not os.path.exists( f'airports/{id}.faa.text.out' ): return
+
+    #--------------------------------------------------------------
+    # Parse the more detailed faa.text.data information that is not
+    # available in the .csv files and insert it back into 
+    # the rawdata for this airport.  
+    #--------------------------------------------------------------
+    T = open( f'airports/{id}.faa.text.out' )
+    info = rawdata[id]
+    info['freqs'] = []
+    info['navaids'] = []
+    multi_freqs_kind = ''
+    while True:
+        line = T.readline()
+        if line == '': break
+
+        if multi_freqs_kind != '' and match( line, r'^\s+\|\s*(\S.*\S)\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)?' ):
+            info['freqs'].append( { 'kind': multi_freqs_kind, 'freq': m.group(1), 'subkind': m.group(2), 'remark': m.group(3) } )
+            continue
+        multi_freqs_kind = ''
+
+        if match( line, r'^(UNICOM|CTAF|ATIS):\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)?' ):
+            freq = m.group(2) 
+            if freq == 'None': freq = ''
+            remarks = m.group(3)
+            if not remarks: remarks = ''
+            info['freqs'].append( { 'kind': m.group(1), 'freq': freq, 'remarks': remarks } )
+        elif match( line, r'^(\S.*\S) (APPROACH|DEPARTURE|DELIVERY|GROUND|TOWER):\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)?' ):
+            multi_freqs_kind = m.group( 1 ) + ' ' + m.group( 2 )
+            remarks = m.group(5)
+            if not remarks: remarks = ''
+            info['freqs'].append( { 'kind': multi_freqs_kind, 'freq': m.group(3), 'subkind': m.group(4), 'remarks': remarks } )
+        elif match( line, r'^(DME|NDB|VOR/DME|VORTAC|TACAN)\s*\|\s*(\w+)\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)\s*\|\s*(\S.*\S)?' ):
+            remarks = m.group(8)
+            if not remarks: remarks = ''
+            info['navaids'].append( { 'kind': m.group(1), 'id': m.group(2), 'name': m.group(3), 'freq': m.group(4), 'hours': m.group(5), 
+                                      'distance': m.group(6), 'bearing': m.group(7), 'remarks': remarks } )
+
+    T.close()
 
 def read():
     print( f'Reading NfdcRunways.csv...' )
@@ -98,28 +159,9 @@ def read():
                             'runways':   runways[row[0]] if row[0] in runways else [] }
 
             #--------------------------------------------------------------
-            # Download more detailed information from faa.gov, but don't 
-            # do this if we have alread done it for a particular airport.
-            # Convert html to text and write to the .faa.text.out file.
+            # Download and parse more detailed information from faa.gov.
             #--------------------------------------------------------------
-            if not os.path.exists( f'airports/{id}.faa.out' ):
-                for t in range(10):
-                    time.sleep( 1 )
-                    c = f'wget -O - https://nfdc.faa.gov/nfdcApps/services/ajv5/airportDisplay.jsp?airportId={id} > airports/{id}.faa.out'
-                    print( c )
-                    info = subprocess.run( c, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
-                    if info.returncode == 0: break
-                    cmd( f'rm -f airports/{id}.faa.out' )
-                    if t == 9: print( f'could not get FAA airport data for {id}' )
-            if os.path.exists( f'airports/{id}.faa.out' ) and not os.path.exists( f'airports/{id}.faa.text.out' ):
-                cmd( f'html2text airports/{id}.faa.out > airports/{id}.faa.text.out' )
-
-            #--------------------------------------------------------------
-            # Parse the more detailed faa.text.data information that is not
-            # available in the .csv files and insert it back into 
-            # the rawdata for this airport.  
-            #--------------------------------------------------------------
-            #if os.path.exists( f'airports/{id}.faa.text.out' ):
+            parse_faa_text( id )
 
         csv_file.close()
 
